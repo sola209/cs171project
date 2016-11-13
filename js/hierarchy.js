2d7fbe304d36e4839e86a70dda77e26465aa5c0c
jsm09 <lware96@gmail.com>
2016-11-13 16:33:29 -0500

    Added Anthony's viz

--- js/hierarchy.js
+++ js/hierarchy.js
@@ -17,11 +17,11 @@ var tree = d3.layout.tree()
 
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
-
-var svgTree = d3.select("#tree").append("svg")
+var svg = d3.select("#tree").append("svg")
     .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+    .attr("height", height + margin.top + margin.bottom);
+
+var svgTree = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
@@ -37,39 +37,68 @@ d3.json("data/hierarchy.json", function(data) {
     console.log(links);
     // Nodes
     var node = svgTree.selectAll("g.node")
-        .data(nodes, function(d) {
-            return d.id || (d.id = ++i); });
+        .data(nodes, function (d) {
+            return d.id || (d.id = ++i);
+        });
 
     var nodeEnter = node.enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function(d) {
-            console.log(d.name)
-            if (d.name=="Organ Trade") {d.x+=60};
-            return "translate(" + d.y+ "," + d.x  + ")"; })
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                console.log(d.name)
+                if (d.name == "Organ Trade") {
+                    d.x += 60
+                }
+                ;
+                return "translate(" + d.y + "," + d.x + ")";
+            })
         ;
 
     nodeEnter.append("circle")
         .attr("r", 10)
-        .attr("fill", function(d) {
+        .attr("fill", function (d) {
             return d.children ? "white" : "steelblue"
         });
     nodeEnter.append("text")
-        .attr("x", function(d) {
-            return d.depth<2 ? 0 : 20; })
-        .attr("dy", function(d) {
-            if (d.depth<2) {return "-1.5em"}
-            else {return "0.35em"}})
-        .style("text-anchor", function(d) {
-            return d.depth<2 ? "middle" : "start"; })
-        .text(function(d) { return d.name; })
+        .attr("x", function (d) {
+            return d.depth < 2 ? 0 : 20;
+        })
+        .attr("dy", function (d) {
+            if (d.depth < 2) {
+                return "-1.5em"
+            }
+            else {
+                return "0.35em"
+            }
+        })
+        .style("text-anchor", function (d) {
+            return d.depth < 2 ? "middle" : "start";
+        })
+        .text(function (d) {
+            return d.name;
+        })
         .style("fill-opacity", 1);
 
     // Links
     var link = svgTree.selectAll("path.link")
-        .data(links, function(d) { return d.target.id; });
+        .data(links, function (d) {
+            return d.target.id;
+        });
 
     link.enter().insert("path", "g")
         .attr("class", "link")
         .attr("d", diagonal);
+
+    // Create tooltips
+    var myTip = d3.tip().attr('class', 'd3-tip').html(function (d) {
+        return "<strong><span >" + d.description + "</strong></span>";
+
+    });
+    svg.call(myTip);
+
+    node.on('mouseover', myTip.show)
+        .on('mouseout', myTip.hide);
+
+
+
 
 });
---
+++ js/intro.js
@@ -0,0 +1,79 @@
+
+
+
+var width = 500,
+    height = 500;
+
+var countries;
+var projection = d3.geo.orthographic()
+    .translate([width / 2, height / 2])
+    .scale(width / 2 - 20)
+    .clipAngle(90)
+    .precision(0.6);
+var canvas = d3.select("#map-area").append("canvas")
+    .attr("width", width)
+    .attr("height", height);
+var c = canvas.node().getContext("2d");
+var path = d3.geo.path()
+    .projection(projection)
+    .context(c);
+var title = d3.select("h5");
+var title2 = d3.select("h6");
+queue()
+    .defer(d3.json, "data/world-110m.json")
+    .defer(d3.json, "data/introData.json")
+    .await(ready);
+function ready(error, world, intro) {
+    if (error) throw error;
+
+
+    var names = intro.countryQuotes;
+
+    countries = topojson.feature(world, world.objects.countries).features;
+    var globe = {type: "Sphere"},
+        land = topojson.feature(world, world.objects.land),
+        borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
+        i = -1,
+
+    countries = countries.filter(function(d) {
+        return names.some(function(n) {
+            if (d.id == n.id){
+                n.geoData = d;
+                return d.name = n.name;
+            }
+
+
+        });
+    });
+
+
+    n = names.length;
+
+    (function transition() {
+        d3.transition()
+            .duration(5000)
+            .each("start", function() {
+                title.text(names[i = (i + 1) % n].name);
+                title2.text(names[i % n].quote);
+            })
+            .tween("rotate", function() {
+                var p = d3.geo.centroid(names[i].geoData),
+                    r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+                return function(t) {
+                    projection.rotate(r(t));
+                    c.clearRect(0, 0, width, height);
+                    c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
+                    c.fillStyle = "#f00", c.beginPath(), path(names[i].geoData), c.fill();
+                    c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+                    c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+                };
+            })
+            .transition()
+            .each("end", transition);
+
+    })();
+
+}
+d3.select(self.frameElement).style("height", height + "px");
+
+/*  Used http://bl.ocks.org/mbostock/4183330  as a guide*/
