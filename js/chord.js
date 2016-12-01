d411fab8a95ca3d70b78edf798eeaf7eff79486a
Omar A <omar.abboud7@gmail.com>
2016-11-30 21:41:58 -0500

    Update chord.js

--- js/chord.js
+++ js/chord.js
@@ -1,8 +1,6 @@
 /**
  * Created by omarabboud1 on 2016-11-27.
  */
-
-<<<<<<< HEAD
 function initChord(){
 
     var width = 700,
@@ -124,119 +122,6 @@ initChord();
 function updateChord() {
     d3.select("#chordsvg").remove();
     initChord();
-=======
-var width = 700,
-    height = 700,
-    outerRadius = Math.min(width, height) / 2 - 10,
-    innerRadius = outerRadius - 24;
-
-var matrix = [
-    [ 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-    [ 14, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0 ],
-    [ 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-    [ 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-    [ 7, 0, 0, 0, 94, 3, 0, 1, 0, 0, 0 ],
-    [ 1, 0, 0, 0, 4, 59, 0, 1, 0, 0, 0 ],
-    [ 1, 0, 0, 0, 0, 0, 97, 10, 0, 0, 0 ],
-    [ 1, 0, 0, 0, 0, 0, 0, 31, 0, 0, 0 ],
-    [ 1, 0, 0, 0, 1, 7, 3, 18, 0, 0, 0 ],
-    [ 7, 0, 0, 0, 1, 25, 3, 33, 0, 0, 0 ],
-    [ 4, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0 ]
-];
-
-var regions = [ "W. Europe", "Cen. Europe", "The Balkans", "West Africa", "South America",
-    "North / Central America", "Sub-Saharan Africa", "North Africa & M.E.", "South Asia",
-    "East Asia", "C.Asia" ];
-
-
-var colors = [ "#006837","#1a9850","#a6d96a","#fdae61","#f46d43",
-    "#66bd63","#a50026","#d73027","#ffffbf","#fee08b","#d9ef8b" ]
-
-var formatPercent = d3.format(".1%");
-
-var arc = d3.svg.arc()
-    .innerRadius(innerRadius)
-    .outerRadius(outerRadius);
-
-var layout = d3.layout.chord()
-    .padding(.04)
-    .sortSubgroups(d3.descending)
-    .sortChords(d3.ascending);
-
-var path = d3.svg.chord()
-    .radius(innerRadius);
-
-var svg = d3.select("#container").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("id", "circle")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-svg.append("circle")
-    .attr("r", outerRadius);
-
-
-// Compute the chord layout.
-layout.matrix(matrix);
-
-// Add a group per neighborhood.
-var group = svg.selectAll(".group")
-    .data(layout.groups)
-    .enter().append("g")
-    .attr("class", "group")
-    .on("mouseover", mouseover);
-
-// Add a mouseover title.
-group.append("title").text(function(d, i) {
-    return regions[i] + ": " + formatPercent(d.value) + " of origins";
-});
-
-// Add the group arc.
-var groupPath = group.append("path")
-    .attr("id", function(d, i) { return "group" + i; })
-    .attr("d", arc)
-    .style("fill", function(d, i) { return colors[i]; });
-
-// Add a text label.
-var groupText = group.append("text")
-    .attr("x", 6)
-    .attr("dy", 15);
-
-groupText.append("textPath")
-    .attr("xlink:href", function(d, i) { return "#group" + i; })
-    .text(function(d, i) { return regions[i]; });
-
-// Remove the labels that don't fit. :(
-groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength(); })
-    .remove();
-
-// Add the chords.
-var chord = svg.selectAll(".chord")
-    .data(layout.chords)
-    .enter().append("path")
-    .attr("class", "chord")
-    .style("fill", function(d) { return colors[d.source.index]; })
-    .attr("d", path);
-
-// Add an elaborate mouseover title for each chord.
-chord.append("title").text(function(d) {
-    return regions[d.source.index]
-        + " → " + regions[d.target.index]
-        + ": " + formatPercent(d.source.value)
-        + "\n" + regions[d.target.index]
-        + " → " + regions[d.source.index]
-        + ": " + formatPercent(d.target.value);
-});
-
-
-function mouseover(d, i) {
-    chord.classed("fade", function(p) {
-
-        return p.source.index != i
-            && p.target.index != i;
-    });
->>>>>>> omar_branch
 }
 
 function update_text(region){
